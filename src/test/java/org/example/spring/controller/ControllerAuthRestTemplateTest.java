9c02e788e135d15c01e123e2731574e912926919
timur <timurpetrov1875@gmail.com>
2025-01-08 19:36:57 +0300

    проверка работы авторизации через RestTemplate

---
+++ src/test/java/org/example/spring/controller/ControllerAuthRestTemplateTest.java
@@ -0,0 +1,59 @@
+package org.example.spring.controller;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootTest
+
+public class ControllerAuthRestTemplateTest {
+    private static RestTemplate restTemplate;
+
+    @BeforeAll
+    public static void setup() {
+        restTemplate = new RestTemplate();
+
+    }
+
+    @Test
+    public void testUnauthorizedAccessToUnsecuredEndpoint() {
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:9999/example-application/unsecured/test", String.class);
+        Assertions.assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        Assertions.assertEquals("unsecured", response.getBody());
+    }
+
+    @Test
+    public void testAuthorizedAccessToUnsecuredEndpoint() {
+        String url = "http://localhost:9999/example-application/secured/test";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth("user", "password");
+        HttpEntity<String> request = new HttpEntity<>(headers);
+        ResponseEntity<String> response = new RestTemplate().exchange(url, HttpMethod.GET, request, String.class);
+        Assertions.assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        Assertions.assertEquals("unsecured", response.getBody());
+    }
+
+    @Test
+    public void testUnauthorizedAccessToSecuredEndpoint() {
+        try {
+            restTemplate.getForEntity("http://localhost:9999/example-application/secured/test", String.class);
+        } catch (HttpClientErrorException e) {
+            Assertions.assertEquals(HttpStatusCode.valueOf(401),e.getStatusCode());
+        }
+    }
+
+    @Test
+    public void testAuthorizedAccessToSecuredEndpoint() {
+        String url = "http://localhost:9999/example-application/secured/test";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBasicAuth("user", "password");
+        HttpEntity<String> request = new HttpEntity<>(headers);
+        ResponseEntity<String> response = new RestTemplate().exchange(url, HttpMethod.GET, request, String.class);
+        Assertions.assertEquals(HttpStatusCode.valueOf(200), response.getStatusCode());
+        Assertions.assertEquals("secured", response.getBody());
+    }
+}
