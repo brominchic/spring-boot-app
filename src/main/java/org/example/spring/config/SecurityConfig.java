154b8f2ae5edb7d01d1b8582890c495d89232e79
timur <timurpetrov1875@gmail.com>
2025-01-24 23:12:50 +0500

    бэкап

--- src/main/java/org/example/spring/config/SecurityConfig.java
+++ src/main/java/org/example/spring/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -30,7 +31,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((auths) -> auths.requestMatchers("/unsecured/**").permitAll().anyRequest().authenticated()).
+        http.authorizeHttpRequests((auths) -> auths.requestMatchers("/unsecured/**").permitAll().
+                requestMatchers("/secured/**").hasRole("User")).
                 httpBasic(Customizer.withDefaults());
         return http.build();
     }
