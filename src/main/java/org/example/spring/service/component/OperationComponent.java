512a0924668d19401d0fc09728c36d168a7e6ff4
timur <timurpetrov1875@gmail.com>
2025-02-17 16:12:53 +0500

    добавил кеш в компонент

--- src/main/java/org/example/spring/service/component/OperationComponent.java
+++ src/main/java/org/example/spring/service/component/OperationComponent.java
@@ -6,18 +6,28 @@ import org.example.spring.model.entity.OperationEntity;
 import org.example.spring.repositories.OperationRepository;
 import org.springframework.stereotype.Component;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 @Component
 @RequiredArgsConstructor
 public class OperationComponent {
     private final CurrencyClient client;
     private final OperationRepository repository;
+    private ConcurrentHashMap<String, String> currencies;
 
     public void createRow(Long sum, String currency) {
-        String code = client.getAll().get(currency);
+        if (currencies == null) {
+            fillTheMap();
+        }
+        String code = currencies.get(currency);
         repository.save(OperationEntity.
                 builder().
                 sum(sum).
                 currencyCode(code)
                 .build());
+    }
+
+    private void fillTheMap() {
+        currencies = new ConcurrentHashMap<>(client.getAll());
     }
 }
