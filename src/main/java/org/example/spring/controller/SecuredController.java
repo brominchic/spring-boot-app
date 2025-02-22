e5ed5c358b6294e68c08ba6edf2f838a9de8c374
timur <timurpetrov1875@gmail.com>
2025-02-23 01:43:35 +0500

    добавлены тесты с компонентами

--- src/main/java/org/example/spring/controller/SecuredController.java
+++ src/main/java/org/example/spring/controller/SecuredController.java
@@ -2,10 +2,10 @@ package org.example.spring.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.example.spring.model.dto.OperationCreateDto;
-import org.example.spring.service.component.OperationComponent;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
