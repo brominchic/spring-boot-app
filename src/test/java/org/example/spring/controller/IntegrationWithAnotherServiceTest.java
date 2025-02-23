31b3dd4b544c8b1337982d21f57b1ee4b11f6d4b
timur <timurpetrov1875@gmail.com>
2025-02-23 20:15:28 +0500

    Исправление теста с restTemplate

--- src/test/java/org/example/spring/controller/IntegrationWithAnotherServiceTest.java
+++ src/test/java/org/example/spring/controller/IntegrationWithAnotherServiceTest.java
@@ -17,8 +17,6 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
@@ -69,10 +67,11 @@ public class IntegrationWithAnotherServiceTest extends SpringBootApplicationTest
         for (int i = 0; i < 100; i++) {
             HttpHeaders headers = new HttpHeaders();
             headers.setContentType(MediaType.APPLICATION_JSON);
-            MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-            body.add("sum", 1);
-            body.add("currency", "rub");
-            HttpEntity<MultiValueMap<String, Object>> requestEntity = new HttpEntity<>(body, headers);
+            String json = "{" +
+                    "\"sum\":1," +
+                    "\"currency\":\"rub\"" +
+                    "}";
+            HttpEntity<String> requestEntity = new HttpEntity<>(json, headers);
             ResponseEntity<String> uploadResponse = restTemplate.postForEntity(uploadUrl, requestEntity, String.class);
         }
         List<OperationEntity> result = new ArrayList<>();
