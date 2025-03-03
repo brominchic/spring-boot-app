b067bd348692935514e7bcc1e3198fc2274124ca
timur <timurpetrov1875@gmail.com>
2025-03-04 00:09:20 +0500

    Замена кэша на cacheable от спринга

--- src/main/java/org/example/spring/service/component/OperationComponent.java
+++ src/main/java/org/example/spring/service/component/OperationComponent.java
@@ -1,31 +1,26 @@
 package org.example.spring.service.component;
 
-import com.github.benmanes.caffeine.cache.Cache;
-import com.github.benmanes.caffeine.cache.Caffeine;
 import lombok.RequiredArgsConstructor;
 import org.example.spring.clients.CurrencyClient;
 import org.example.spring.model.entity.OperationEntity;
 import org.example.spring.repositories.OperationRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
 
 @Component
 @RequiredArgsConstructor
 public class OperationComponent {
     private final CurrencyClient client;
     private final OperationRepository repository;
-    private final Cache<String, String> currencies = Caffeine.newBuilder()
-            .expireAfterWrite(1, TimeUnit.MINUTES)
-            .maximumSize(100)
-            .build();
+    private final HashMap<String, String> currencies;
 
     public void createRowWithCache(Long sum, String currency) {
-        if (currencies.asMap().isEmpty()) {
+        if (currencies.isEmpty()) {
             fillTheMap();
         }
-        String code = currencies.getIfPresent(currency);
+        String code = currencies.get(currency);
         repository.save(OperationEntity.
                 builder().
                 sum(sum).
@@ -42,11 +37,9 @@ public class OperationComponent {
                 .build());
     }
 
+    @Cacheable
     private void fillTheMap() {
-        currencies.cleanUp();
         HashMap<String, String> data = client.getAll();
-        for (HashMap.Entry<String, String> str : data.entrySet()) {
-            currencies.put(str.getKey(), str.getValue());
-        }
+        currencies.putAll(data);
     }
 }
